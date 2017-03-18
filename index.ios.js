6f1243926509e7dc3d659819fcd08bbe00db3aea
oliverlai55 <oliverlai55@gmail.com>
2017-03-18 18:43:05 -0400

    Update file

---
+++ index.ios.js
@@ -0,0 +1,9 @@
+// place code here for iOS
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a Component
+
+// Render it to the device

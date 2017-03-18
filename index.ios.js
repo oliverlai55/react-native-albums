2a5cd7492b8697d761c6c503bec25e5ebd19d667
oliverlai55 <oliverlai55@gmail.com>
2017-03-18 18:56:34 -0400

    Write functional component

--- index.ios.js
+++ index.ios.js
@@ -5,5 +5,10 @@ import React from 'react';
 import ReactNative from 'react-native';
 
 // Create a Component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
 // Render it to the device

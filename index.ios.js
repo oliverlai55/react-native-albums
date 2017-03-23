6fad210a06b8c227cb26c8f4185ca15016c14ae7
oliverlai55 <oliverlai55@gmail.com>
2017-03-23 19:17:30 -0400

    Make page scrollable

--- index.ios.js
+++ index.ios.js
@@ -8,13 +8,13 @@ import AlbumList from './src/components/AlbumList';
 
 // Create a Component
 const App = () => (
-    <View>
+    <View style={{ flex: 1 }}>
       <Header headerText={'Albums'} />
       <AlbumList />
     </View>
 );
 
 
-// Render it to the device 
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
 // Register component to the application
